2353336525dd8c05d769ab07b57b3144a402ca14
4epB9Ik <mat.one777@gmail.com>
2025-09-21 18:30:45 +0300

    push plugin code

---
+++ src/main/java/net/dev_4epb9ik/sponsor_cmd/SponsorCmd.java
@@ -0,0 +1,17 @@
+package net.dev_4epb9ik.sponsor_cmd;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SponsorCmd extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
